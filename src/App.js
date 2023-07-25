737e3413c0befed4fae9d342139ba4137ee1c2a2
Mohammad Usman Zafar <usmanzafar622@gmail.com>
2023-07-25 15:42:15 +0500

    Todo Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import Increment from "./Component/Increment";
+import TodoList from "./Component/Todo";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Increment />
-        <Increment />
+        <TodoList />
       </header>
     </div>
   );
